ebd8089792b11de6c100de0c24ab8ff1a2d5d6be
Platon <nebunu_zzz@yahoo.com>
2021-11-02 21:28:13 +0200

    new

---
+++ test.py
@@ -0,0 +1,50 @@
+# ctr + / ==> #
+# print(9/4)
+# print(9 // 4)
+# print(9 % 4)
+# print(1 + 3 + 2.5 )
+
+# print(True)  - boolean
+# print(False)
+
+# None - ( ne create, ne initializate )
+
+# print(7 is None)
+# print(7 == 4)
+# print(7 == 7)
+
+# x = 7.5
+# print(x, type(x))
+
+# var1 = 3
+# var2 = 3
+# print(var1 is not var2)
+
+# print(1 not in [1,2,3])
+
+# print(bin(6 &7))
+
+# nume variabile si nume functii (snake_case)
+
+# MY_NAME = "aDRIAN"
+# MY_AGE = 26
+# print(id(MY_AGE))
+# print(id(MY_NAME))
+
+# my_number = 20
+# my_number = my_number + 5
+# my_number += 5
+# print(my_number)
+
+
+# print(chr(97), chr(65))
+
+"""comentariu
+pe mai multe
+linii"""
+
+
+# print(r"haide sa mergem \n buuuu")
+nume = "Adrian"
+print("Numele meu este {name} si am {age}" .format(name=nume, age=35))
+
